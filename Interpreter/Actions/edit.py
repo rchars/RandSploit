a854d7b5bc05da50b4f28dfd657a119a7e99a2c7
rchars <rchars@protonmail.com>
2023-10-13 19:40:12 +0200

    Added edit option

---
+++ Interpreter/Actions/edit.py
@@ -0,0 +1,22 @@
+import Interpreter.state as state
+import os
+
+
+def call_editor(mod_str_path):
+	if mod_str_path.isdigit():
+		mod_index = int(mod_str_path)
+		for index, mod_path in state.MOD_HANDLER.iter_mods_with_index():
+			if index == mod_index:
+				mod_str_path = str(mod_path)
+				break
+		else: raise ValueError('No module has index \'{mod_index}\' assigned')
+	if not state.EDITOR:
+		state.EDITOR = input('Editor:')
+	os.system(f'{state.EDITOR} {mod_str_path}')
+
+
+def execute(module):
+	if module: call_editor(module)
+	elif not state.MOD_HANDLER.is_mod_set():
+		call_editor(state.MOD_HANDLER.active_mod_path)
+	else: print('Choose a module first.')
